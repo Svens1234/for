3f8aae06b2225051d711713b3bdc1c500522390f
Svenden123 <buhrus1@yandex.ru>
2021-08-11 20:51:17 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,86 @@
+#mylist = [1,2,3,4,5,6,7,8,9,10]
+#for num in mylist:
+    #print('hello')
+
+
+#mylist = [1,2,3,4,5,6,7,8,9,10]
+#for num in mylist:
+    #if num % 2 == 0:
+        #print(num)
+    #else:
+        #print(f' Odd Number: {num}')
+
+
+#mylist = [1,2,3,4,5,6,7,8,9,10]
+#list_num = 0
+
+#for num in mylist:
+    #list_num = list_num + num
+
+    #print(list_num)
+
+
+#mylist = [1,2,3,4,5,6,7,8,9,10]
+#list_num = 0
+
+#for num in mylist:
+    #list_num = list_num + num
+
+#print(list_num)
+
+
+#mystring = "Hello world"
+#for letter in mystring:
+    #print(letter)
+
+
+#for _ in "blabla":
+    #print('Cool!')
+
+#tup=(1,2,3)
+
+#for item in tup:
+    #print(item)
+
+
+
+
+
+#mylist = [(1,2), (3,4), (5,6), (7,8)]
+#print(len(mylist))
+
+#for item in mylist:
+    #print(item)
+
+
+#for (a,b) in mylist:
+    #print(a)
+    #print(b)
+
+
+#mylist = [(1,2,3), (4,5,6), (7,8,9)]
+#for item in mylist:
+    #print(item)
+
+#for a,b,c in mylist:
+    #print(a)
+    #print(b)
+    #print(c)
+
+
+d = {'k1':1, 'k2':2, 'k3':3}
+
+for item in d.items():
+    print(item)
+
+
+for key, value in d.items():
+    print(value)
+
+
+for value in d.values():
+    print(value)
+
+
+
+
